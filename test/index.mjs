cbf5b3198493d025c24ba192b206a7c1f6c28c62
singhritesh85 <56765895+singhritesh85@users.noreply.github.com>
2023-03-23 12:33:06 +0530

    Update index.mjs

--- test/index.mjs
+++ test/index.mjs
@@ -2,7 +2,7 @@ export const handler = async(event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda 123!'),
+        body: JSON.stringify('Hello from Lambda 123 Tetra!'),
     };
     return response;
 };
